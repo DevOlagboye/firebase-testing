bc12dc9ecf7d5cca3ee84938523eeae46788b856
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-17 19:13:14 +0100

    Started working on the Login Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
     <div className="App">
-     
+     <Login/>
     </div>
   );
 }
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
+const Login = () => {
+  const [data, setData] = useState([]);
+  const handleInput = (event) => {
+    let newInput = { [event.target.name]: event.target.value };
 
-const Login = () =>{
-    return (
-        <div></div>
-    )
-}
+    setData({ ...data, ...newInput });
+  };
+  const handleSubmit = () => {};
+  return (
+    <div className="login-container">
+      <input
+        name="email"
+        placeholder="Input Email"
+        onChange={(event) => handleInput(event)}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Input Password"
+        onChange={(event) => handleInput(event)}
+      />
+    </div>
+  );
+};
 
-export default Login
+export default Login;
