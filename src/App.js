50030b82da41b60d9223246d19d53d35b093b73c
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-18 18:10:43 +0100

    Passing States with Context API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Login from './Components/Login/Login';
-import LoggedIn from './Components/LoggedIn/LoggedIn';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Components/Login/Login";
+import LoggedIn from "./Components/LoggedIn/LoggedIn";
+import { createContext } from "react";
 
 function App() {
   return (
     <div className="App">
-     <Router>
-     <Routes>
-      <Route exact path="/" element={<Login/>}/>
-      <Route exact path="/loggedIn" element={<LoggedIn/>}/>
-     </Routes>
-     
-     </Router>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/loggedIn" element={<LoggedIn />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
