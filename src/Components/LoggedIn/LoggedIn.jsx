d25ceab694c7d54363b1142402dc0d7dcfbfcf66
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-19 19:05:38 +0100

    Making some changes

--- src/Components/LoggedIn/LoggedIn.jsx
+++ src/Components/LoggedIn/LoggedIn.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useContext, useNavigate } from "react";
+import { useContext } from "react";
+import {useNavigate} from "react-router-dom"
 import { UserContext } from "../../UserContext";
 
 const LoggedIn = () => {
