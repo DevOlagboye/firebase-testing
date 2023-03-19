a29499776afa95317003f9e7de0a62025cce858f
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-19 19:02:59 +0100

    Making some changes

--- src/Components/LoggedIn/LoggedIn.jsx
+++ src/Components/LoggedIn/LoggedIn.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import { useContext } from "react";
+import React from "react";
+import { useContext, useNavigate } from "react";
 import { UserContext } from "../../UserContext";
 
 const LoggedIn = () => {
-    const {data, setData} = useContext(UserContext)
-    const logOut = () =>{
-        setData(false)
-       }
+  const { data, setData } = useContext(UserContext);
+  const navigate = useNavigate()
+  const logOut = () => {
+    setData(false);
+    navigate("/")
+  };
   return (
     <div>
-        <h5>LoggedIn</h5>
-        
-        {data ? <button onClick={logOut}>Logout</button>: <button>Test</button>}
+      <h5>LoggedIn</h5>
+      {data ? <button onClick={logOut}>Logout</button> : <button>Test</button>}
     </div>
-  )
-}
+  );
+};
 
-export default LoggedIn
+export default LoggedIn;
