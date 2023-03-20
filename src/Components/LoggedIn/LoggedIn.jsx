a0a5ed69785cc050be9c9cb4105ee56408b50e71
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-20 15:36:08 +0100

    Finished Configuring the Success Message on login

--- src/Components/LoggedIn/LoggedIn.jsx
+++ src/Components/LoggedIn/LoggedIn.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { useContext } from "react";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../UserContext";
 
 const LoggedIn = () => {
   const { data, setData } = useContext(UserContext);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const logOut = () => {
     setData(false);
-    navigate("/")
+    navigate("/");
   };
   return (
     <div>
-      <h5>LoggedIn</h5>
+      {data ? <h5>LoggedIn</h5>: <h5>You need to kindly Login to reach Here</h5>}
       {data ? <button onClick={logOut}>Logout</button> : <button>Test</button>}
     </div>
   );
