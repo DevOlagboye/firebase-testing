61682a7688b7092a87a6b150547fc2002f0840cd
Olagboye Seyi <akingbolaola@gmail.com>
2023-03-22 11:30:09 +0100

    Requiring login state to navigate to some pages

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Components/Login/Login";
 import LoggedIn from "./Components/LoggedIn/LoggedIn";
-import { UserContext } from "./UserContext";
+import { UserContext, logInContext } from "./UserContext";
 
 function App() {
   const [data, setData] = useState([]);
+  const [loggedIn, setLoggedIn] = useState([])
   return (
     <div className="App">
       <Router>
       <UserContext.Provider value={{data, setData}}>
+        <logInContext.Provider value={{loggedIn, setLoggedIn}}>
         <Routes> 
           <Route exact path="/" element={<Login />} />
           <Route exact path="/loggedIn" element={<LoggedIn />} />
         </Routes>
+        </logInContext.Provider>
         </UserContext.Provider>
       </Router>
     </div>
